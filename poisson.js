3c6e24df7be1f904b39ba8a9a374f5c35b2331d3
Andrés <angarg12@gmail.com>
2017-05-30 19:24:42 +0200

    Initial commit

---
+++ poisson.js
@@ -0,0 +1,14 @@
+function Poisson(){
+  function getPoisson(lambda) {
+	let L = Math.exp(-lambda);
+	let p = 1.0;
+	let k = 0;
+
+	do {
+	  k++;
+	  p *= Math.random();
+	} while (p > L);
+
+	return k - 1;
+  }
+}
